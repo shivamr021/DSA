8b3f1fc2c3ff3e179a8fc8eddd81be2611fd1543
Shivam <146384849+shivamr021@users.noreply.github.com>
2025-06-15 16:01:27 +0530

    Create Square Pattern.py

---
+++ Coding Ninjas/Square Pattern.py
@@ -0,0 +1,8 @@
+## Read input as specified in the question
+## Print the required output in given format
+
+N = int(input())
+for i in range(N):
+    for j in range(N):
+        print(N, end='')
+    print()
