ea3dddd1f5651573f4d0e01a94c369365bb39465
Shivam Rathod <146384849+shivamr021@users.noreply.github.com>
2025-10-31 21:56:12 +0530

    Create 542. 01 Matrix.py

---
+++ LeetCode/542. 01 Matrix.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        dist = [[float('inf')] * n for _ in range(m)]
+        q = deque()
+        
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    dist[i][j] = 0
+                    q.append((i, j))
+        
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        while q:
+            r, c = q.popleft()
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n:
+                    if dist[nr][nc] > dist[r][c] + 1:
+                        dist[nr][nc] = dist[r][c] + 1
+                        q.append((nr, nc))
+        
+        return dist
