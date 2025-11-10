39fb4d2cdd423d041f7e1f1ecfae3f5711dcd7e9
Shivam Rathod <146384849+shivamr021@users.noreply.github.com>
2025-11-10 23:21:56 +0530

    Create Binary Search.py

---
+++ Coding Ninjas/Binary Search.py
@@ -0,0 +1,15 @@
+def search(nums: [int], target: int):
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
