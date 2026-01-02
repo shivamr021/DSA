f5e428ce304da5084d91884ae34fcc7dd2d992ff
Shivam Rathod <146384849+shivamr021@users.noreply.github.com>
2026-01-02 15:07:54 +0530

    Create 961. N-Repeated Element in Size 2N Array.py

---
+++ LeetCode/961. N-Repeated Element in Size 2N Array.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        seen = set()
+        for x in nums:
+            if x in seen:
+                return x
+            seen.add(x)
