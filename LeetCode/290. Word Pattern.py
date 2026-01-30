1f44a6deb64f8630fd4338693388147529c99dc8
Shivam Rathod <146384849+shivamr021@users.noreply.github.com>
2026-01-30 22:01:26 +0530

    Create 290. Word Pattern.py

---
+++ LeetCode/290. Word Pattern.py
@@ -0,0 +1,34 @@
+# Python one liner
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split(' ')
+        if  not len(s) == len(pattern):
+                return False
+        return len(set(zip(pattern, s))) == len(set(s)) == len(set(pattern))
+
+                        # OR
+
+# Interview friendly
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for c, w in zip(pattern, words):
+            if c in char_to_word:
+                if char_to_word[c] != w:
+                    return False
+            else:
+                char_to_word[c] = w
+
+            if w in word_to_char:
+                if word_to_char[w] != c:
+                    return False
+            else:
+                word_to_char[w] = c
+
+        return True
